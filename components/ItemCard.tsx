857eae59a0c986827b03a493169cea18bdd004f9
ajith0110 <santhan.ajith50@gmail.com>
2022-12-17 22:45:54 +0530

    color changed

--- components/ItemCard.tsx
+++ components/ItemCard.tsx
@@ -35,14 +35,6 @@ export default function ItemCard(props: ItemCardProps) {
                         />
                         <span className={styles.cardTitle}>{title}</span>
                     </div>
-                    <div className={styles.priceTagArea}>
-                        <div className={styles.priceTag}>
-                            <span> 01 Person - {personOne} LKR</span>
-                        </div>
-                        <div className={styles.priceTag}>
-                            <span> 02 Person - {personTwo} LKR</span>
-                        </div>
-                    </div>
                     <div className={styles.bulletListArea}>
                         <div className={styles.bulletListItem}>
                             <BsFillCheckCircleFill
@@ -71,6 +63,15 @@ export default function ItemCard(props: ItemCardProps) {
                             </span>
                         </div>
                     </div>
+                    <div className={styles.priceTagArea}>
+                        <div className={styles.priceTag}>
+                            <span> 01 Person - {personOne} LKR</span>
+                        </div>
+                        <div className={styles.priceTag}>
+                            <span> 02 Person - {personTwo} LKR</span>
+                        </div>
+                    </div>
+                  
                 </div>
                 <div className={styles.btnArea}>
                     <button type="button" className={styles.callNowBtn}>
