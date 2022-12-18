78decbbdaacd64e53407e57d071616ff4b8e5e40
ajith0110 <santhan.ajith50@gmail.com>
2022-12-18 20:47:00 +0530

    final changes

--- components/ItemCard.tsx
+++ components/ItemCard.tsx
@@ -4,86 +4,51 @@ import { BsFillCheckCircleFill } from "react-icons/bs";
 import { ImWhatsapp } from "react-icons/im";
 
 export type ItemType = {
-    url?: string;
-    title?: string;
-    personOne?: string;
-    personTwo?: string;
-    additional?: string;
-    whatsApp?: string;
+  url?: string;
+  title?: string;
+  subTitle?: string;
+  personOne?: string;
+  personTwo?: string;
+  additional?: string;
+  whatsApp?: string;
+
 };
 
 interface ItemCardProps {
-    item: ItemType;
+  item: ItemType;
 }
 
 export default function ItemCard(props: ItemCardProps) {
-    const { item } = props;
-    const { title, url, additional, personOne, personTwo, whatsApp } = item;
-    return (
-        <div className={styles.card}>
-            <div
-                style={{
-                    backgroundImage: `url("${url}")`,
-                }}
-                className={styles.cardleftcol}
-            ></div>
-            <div className={styles.cardrighttcol}>
-                <div className={styles.cardrighttcolContentWrap}>
-                    <div className={styles.cardTitleArea}>
-                        <AiOutlineBorderlessTable
-                            className={styles.titleIcon}
-                        />
-                        <span className={styles.cardTitle}>{title}</span>
-                    </div>
-                    <div className={styles.bulletListArea}>
-                        <div className={styles.bulletListItem}>
-                            <BsFillCheckCircleFill
-                                className={styles.listIcon}
-                            />
-                            <span className={styles.listText}>
-                                Free Delivery
-                            </span>
-                        </div>
+  const { item } = props;
+  const { title, url, additional, personOne, personTwo, whatsApp,subTitle } = item;
+  return (
+    <div className={styles.card}>
+      <img
+        src={url}
+        
+        className={styles.image}
+      />
 
-                        <div className={styles.bulletListItem}>
-                            <BsFillCheckCircleFill
-                                className={styles.listIcon}
-                            />
-                            <span className={styles.listText}>
-                                Rs.{additional}/- Per Each Additional Person
-                            </span>
-                        </div>
+    
+        <div className={styles.bottomSection}>
+        
+          <span className={styles.cardTitle}>{title}</span><br></br>
+          <span className={styles.cardSubTitle}>{subTitle}</span>
 
-                        <div className={styles.bulletListItem}>
-                            <BsFillCheckCircleFill
-                                className={styles.listIcon}
-                            />
-                            <span className={styles.listText}>
-                                With Matte Laminated Frame
-                            </span>
-                        </div>
-                    </div>
-                    <div className={styles.priceTagArea}>
-                        <div className={styles.priceTag}>
-                            <span> 01 Person - {personOne} LKR</span>
-                        </div>
-                        <div className={styles.priceTag}>
-                            <span> 02 Person - {personTwo} LKR</span>
-                        </div>
-                    </div>
-                  
-                </div>
-                <div className={styles.btnArea}>
-                    <button type="button" className={styles.callNowBtn}>
-                        <span className={styles.btnIcon}>
-                            <ImWhatsapp />
-                        </span>
-                        <a target="blank" href={`${whatsApp}`}>
-                            Order now
-                        </a>
-                    </button>
-                </div>
-            </div>
+          <button type="button" className={styles.callNowBtn}>
+          <span className={styles.btnIcon}>
+            <ImWhatsapp />
+          </span>
+          <a target="blank" href={`${whatsApp}`}>
+            Order now
+          </a>
+        </button>
         </div>
-    );
+     
+   
+     
+        
+    
+    </div>
+  );
 }
--- data.ts
+++ data.ts
@@ -2,36 +2,40 @@ import { ItemType } from "./components/ItemCard";
 
 const priceDetails: ItemType[] = [
     {
-        title: "5 x 7 Inches",
+        title: "8 x 10 Inches",
+        subTitle:"WOOD FRAME",
         url: "https://images.unsplash.com/photo-1513161455079-7dc1de15ef3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1976&q=80",
         personOne: "1500",
         personTwo: "2500",
         additional: "600",
-        whatsApp: "https://wa.me/c/94774031528",
+        whatsApp: "https://api.whatsapp.com/send?phone=774031528&text=https://wa.me/p/5879241992096791/94774031528",
     },
     {
         title: "6 x 8 Inches",
+        subTitle:"WOOD FRAME",
         url: "https://images.unsplash.com/photo-1632935190457-9f3b8a73d44c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80",
         personOne: "3500",
         personTwo: "4500",
         additional: "600",
-        whatsApp: "https://wa.me/c/94774031528",
+        whatsApp: "https://api.whatsapp.com/send?phone=774031528&text=https://wa.me/p/5821324514592095/94774031528",
     },
     {
-        title: "8 x 12 Inches",
+        title: "12 x 8 Inches",
+        subTitle:"WOOD FRAME",
         url: "https://images.unsplash.com/photo-1567225477277-c8162eb4991d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80",
         personOne: "5500",
         personTwo: "6500",
         additional: "600",
-        whatsApp: "https://wa.me/c/94774031528",
+        whatsApp: "https://api.whatsapp.com/send?phone=774031528&text=https://wa.me/p/5992983677430952/94774031528",
     },
     {
         title: "10 x15 Inches",
+        subTitle:"WOOD FRAME",
         url: "https://images.unsplash.com/photo-1505841468529-d99f8d82ef8f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80",
         personOne: "6500",
         personTwo: "7500",
         additional: "600",
-        whatsApp: "https://wa.me/c/94774031528",
+        whatsApp: "https://api.whatsapp.com/send?phone=774031528&text=https://wa.me/p/8468555759883946/94774031528",
     },
 ];
 
