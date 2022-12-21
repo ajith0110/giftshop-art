7f6e41e0d8bfd4b18f89166ef86e500f16c0f39e
ajith0110 <santhan.ajith50@gmail.com>
2022-12-21 22:38:10 +0530

    images

---
+++ assets/index.ts
@@ -0,0 +1,15 @@
+import four from './4.jpg'
+import fourSix from './6 x 8.jpg'
+import ten from './8 x 10.jpg'
+import tenFive from './10 x 15.jpg'
+import twel from './128.jpg'
+
+
+export{
+   four,
+   fourSix,
+   ten,
+   tenFive,
+   twel
+   
+}
